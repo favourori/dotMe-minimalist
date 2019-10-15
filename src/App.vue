ce3efe018312ad410053cc584058449c088d2b30
Favour Ori <orifavour2000@gmail.com>
2019-10-15 14:40:29 +0200

    minor update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+    <div style=" background-color: rgb(106, 175, 245); height: 4px; width:100%"></div>
     <div id="app">
-      <br />
-      <h1 style="font-size: 55px">Favour ori.</h1>
+      <br /><br/><br/>
+      <h1 style="font-size: 31px; color: grey">Favour ori.</h1>
 
-      <p>I am a Frontend Developer - passionate about developing innovative products geared toward making life pretty easy.</p>
+      <p>I am a <u>Frontend Developer</u> passionate about developing innovative products geared toward making life pretty easy.</p>
       <p>On demand, I write some Backend code (Mostly Node Js).</p>
       <p>
         In my spare time, I teach programming at
@@ -17,10 +18,11 @@
       </p>
       <br/>
       <hr style="border-top: 1px solid #ecf0f1"/>
+      <br/>
       <p>I am always open to opportunies, so feel free to reach out.</p>
       <p>
         <a href="https://github.com/favourori">Github</a> &nbsp;&nbsp;
-        <a href="favourori96@gmail.com">Email</a> &nbsp; &nbsp;
+        <a href="mailto:favourori96@gmail.com">Email</a> &nbsp; &nbsp;
         <a href="https://twitter.com/favorori1">Twitter</a>
       </p>
     </div>
@@ -28,6 +30,7 @@
 </template>
 
 <script>
+
 export default {
   name: "app",
   components: {}
@@ -49,7 +52,7 @@ a:after {
   right: -0.1em;
   bottom: 0;
   transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
-  background-color: #fca6a6;
+  background-color: rgb(106, 175, 245);
 }
 a:hover:after {
   top: 0%;
@@ -64,7 +67,7 @@ a:hover:after {
 }
 
 #app {
-  padding: 0 10px;
+  padding: 0 17px;
   max-width: 900px;
   margin: 0 auto;
 }
